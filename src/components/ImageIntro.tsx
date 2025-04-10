e1ac0c7e0f6c9dacb16c64083360fbde1e8e316f
YashNarK <narenkrithick@gmail.com>
2025-04-10 18:24:37 +0530

    added sign for dark mode

--- src/components/ImageIntro.tsx
+++ src/components/ImageIntro.tsx
@@ -1,50 +1,41 @@
 import { useAppTheme } from "@/hooks/useAppTheme";
-import { Box, Stack, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 
 const ImageIntro = () => {
   const myImageUrl = `url('/img/myImageCropped.png')`;
   const theme = useAppTheme();
-
+  const mySignUrl = `/img/sign-${
+    theme.palette.mode === "light" ? "black" : "white"
+  }.png`;
   return (
-    <>
-      {" "}
-      <Stack
-        direction={"row"}
+    <Box display={"flex"} alignItems={"center"} justifyContent={"center"}>
+      <Box
         sx={{
-          position: "relative", // needed if you want elements to overlap
-          alignItems: "center",
+          position: "relative",
+          width: { xs: "230px", sm: "360px" },
+          height: { xs: "230px", sm: "360px" },
+          backgroundColor: theme.palette.background.paper,
+          backgroundImage: myImageUrl,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          zIndex: 2,
+          borderRadius: "50%",
         }}
       >
         <Box
+          component="img"
+          src={mySignUrl}
+          alt="Signature"
           sx={{
-            position: "relative",
-            width: { xs: "230px", sm: "360px" },
-            height: { xs: "230px", sm: "360px" },
-            backgroundColor: theme.palette.background.paper,
-            backgroundImage: myImageUrl,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            zIndex: 2,
-            borderRadius: "50%",
-            m: "auto",
+            position: "absolute",
+            bottom: "10px",
+            right: "10px",
+            width: { xs: "120px", sm: "200px" },
+            opacity: 0.8, // optional: to blend it better
           }}
-        >
-          {/* Signature Overlay */}
-          <Box
-            component="img"
-            src="/img/sign-black.png"
-            alt="Signature"
-            sx={{
-              position: "absolute",
-              bottom: "10px",
-              right: "10px",
-              width: { xs: "120px", sm: "200px" },
-              opacity: 0.8, // optional: to blend it better
-            }}
-          />
-        </Box>
-      </Stack>
-    </>
+        />
+      </Box>
+    </Box>
   );
 };
 
