def0c9d748874ec523f49a8221aa5747d4e04e87
YashNarK <narenkrithick@gmail.com>
2025-04-11 23:59:07 +0530

    added icon only mode for social buttons

--- src/components/SocialButton.tsx
+++ src/components/SocialButton.tsx
@@ -8,6 +8,7 @@ interface ISocialButtonProps {
   color: string;
   backgroundColor: string;
   href: string;
+  mode?: "full" | "iconOnly";
 }
 
 const SocialButton = ({
@@ -16,20 +17,41 @@ const SocialButton = ({
   color,
   backgroundColor,
   href,
+  mode = "full",
 }: ISocialButtonProps) => {
   return (
-    <Button
-      variant="outlined"
-      startIcon={icon}
-      onClick={() => window.open(href, "_blank")}
-      sx={{
-        width: "100%",
-        color: { color },
-        backgroundColor: { backgroundColor },
-      }}
-    >
-      {text}
-    </Button>
+    <>
+      {mode === "full" ? (
+        <Button
+          variant="outlined"
+          startIcon={icon}
+          onClick={() => window.open(href, "_blank")}
+          sx={{
+            width: "100%",
+            color: { color },
+            backgroundColor: { backgroundColor },
+          }}
+        >
+          {text}
+        </Button>
+      ) : (
+        <Button
+          size="small"
+          variant="outlined"
+          sx={{
+            minWidth: 0,
+            color: { color },
+            width: 40,
+            height: 40,
+            p: 0,
+            backgroundColor: { backgroundColor },
+          }}
+          onClick={() => window.open(href, "_blank")}
+        >
+          {icon}
+        </Button>
+      )}
+    </>
   );
 };
 
