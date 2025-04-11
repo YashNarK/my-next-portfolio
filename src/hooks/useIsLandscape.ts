d5547e31d171b8d8f6dcfbbc05c57ca0d2ebfda0
YashNarK <narenkrithick@gmail.com>
2025-04-12 00:25:42 +0530

    useIsLandscape custom hook defined

---
+++ src/hooks/useIsLandscape.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+const useIsLandscape = () => {
+  const [isLandscape, setIsLandscape] = useState(
+    window.innerWidth > window.innerHeight
+  );
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLandscape(window.innerWidth > window.innerHeight);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return isLandscape;
+};
+export default useIsLandscape;
