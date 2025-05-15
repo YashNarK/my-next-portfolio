3c8561e17313662544468f8131f06074cd379f38
YashNarK <narenkrithick@gmail.com>
2025-05-15 23:38:20 +0530

    firebase client updated to get storage

--- src/lib/firebase/firebase-client.ts
+++ src/lib/firebase/firebase-client.ts
@@ -1,5 +1,6 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +20,4 @@ console.log("Firebase Config", firebaseConfig);
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const storage = getStorage(app);
