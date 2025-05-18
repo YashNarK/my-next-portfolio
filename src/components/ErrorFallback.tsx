574e55e60ababa0612a4039ba83b168717d68492
YashNarK <narenkrithick@gmail.com>
2025-05-18 20:05:50 +0530

    better error component for bounding

--- src/components/ErrorFallback.tsx
+++ src/components/ErrorFallback.tsx
@@ -1,13 +1,15 @@
 // components/ErrorFallback.tsx
 "use client";
 
-import { Box, Button, Typography } from "@mui/material";
+import { alpha, Box, Button, Typography } from "@mui/material";
+import { useRouter } from "next/navigation";
 import { FallbackProps } from "react-error-boundary";
 
 export default function ErrorFallback({
   error,
   resetErrorBoundary,
 }: FallbackProps) {
+  const router = useRouter();
   return (
     <Box
       height={"100vh"}
@@ -16,14 +18,42 @@ export default function ErrorFallback({
       alignItems="center"
       flexDirection="column"
       gap={2}
+      sx={{
+        backgroundImage: "url('/img/Error.jpeg')",
+      }}
     >
-      <Typography variant="h6" color="error">
-        Something went wrong:
-      </Typography>
-      <Typography color="error">{error.message}</Typography>
-      <Button variant="outlined" onClick={resetErrorBoundary}>
-        Try Again
-      </Button>
+      <Box border={1} borderRadius={2} bgcolor={alpha("#808080", 0.5)} p={4}>
+        <Typography
+          variant="h6"
+          color="textPrimary"
+          fontWeight={900}
+          fontSize={20}
+        >
+          Something went wrong:
+        </Typography>
+        <Typography color="textPrimary" fontWeight={900} fontSize={20}>
+          {error.message}
+        </Typography>
+      </Box>
+      <Box display="flex" gap={2}>
+        <Button
+          variant="contained"
+          onClick={resetErrorBoundary}
+          color="primary"
+        >
+          Try Again
+        </Button>
+        <Button
+          variant="contained"
+          color="success"
+          onClick={() => {
+            resetErrorBoundary();
+            router.push("/");
+          }}
+        >
+          Go Back to Home
+        </Button>
+      </Box>
     </Box>
   );
 }
