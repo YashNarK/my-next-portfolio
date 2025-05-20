a0250015045d9d7883be1bc87abc93358ed061b5
YashNarK <narenkrithick@gmail.com>
2025-05-20 13:01:50 +0530

    added SceneBackground to project layout

--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -1,5 +1,12 @@
+import SceneBackground from "@/sharedComponents/Scenebackground";
 import { ReactNode } from "react";
 
 export default function ProjectsLayout({ children }: { children: ReactNode }) {
-  return <>{children}</>;
+  return (
+    <>
+      <SceneBackground />
+
+      {children}
+    </>
+  );
 }
