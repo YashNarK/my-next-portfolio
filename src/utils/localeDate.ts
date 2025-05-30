aee0b357d59c50a73c7d58502226ed7072c4ae1e
YashNarK <narenkrithick@gmail.com>
2025-05-30 19:12:18 +0530

    localeDate util created

---
+++ src/utils/localeDate.ts
@@ -0,0 +1,12 @@
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+export default function localeDate(dateISO_Value: string) {
+  dayjs.extend(utc);
+  dayjs.extend(timezone);
+
+  // Convert UTC to local time
+  const formatted = dayjs.utc(dateISO_Value).local().format("DD/MM/YYYY");
+  return formatted;
+}
