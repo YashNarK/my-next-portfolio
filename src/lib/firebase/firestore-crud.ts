770d5e8786b8f8503d139b0a12ee181a5cc6b871
YashNarK <narenkrithick@gmail.com>
2025-05-30 19:12:06 +0530

    Publications CRUD added

--- src/lib/firebase/firestore-crud.ts
+++ src/lib/firebase/firestore-crud.ts
@@ -1,4 +1,4 @@
-import { IProject } from "../../../data/data.type";
+import { IProject, IPublication } from "../../../data/data.type";
 import { db } from "./firebase-client";
 import {
   collection,
@@ -9,8 +9,8 @@ import {
   deleteDoc,
 } from "firebase/firestore";
 
+// Projects CRUD
 const projectsCollection = collection(db, "projects");
-
 export async function getAllProjects(): Promise<(IProject & { id: string })[]> {
   const snapshot = await getDocs(projectsCollection);
   return snapshot.docs.map((doc) => ({
@@ -34,5 +34,37 @@ export async function updateProject(
 
 export async function deleteProject(id: string): Promise<void> {
   const docRef = doc(db, "projects", id);
+  await deleteDoc(docRef);
+}
+
+// Publications CRUD
+const publicationsCollection = collection(db, "publications");
+export async function getAllPublications(): Promise<
+  (IPublication & { id: string })[]
+> {
+  const snapshot = await getDocs(publicationsCollection);
+  return snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...(doc.data() as IPublication),
+  }));
+}
+
+export async function addPublication(
+  publication: IPublication
+): Promise<string> {
+  const docRef = await addDoc(publicationsCollection, publication);
+  return docRef.id;
+}
+
+export async function updatePublication(
+  id: string,
+  publiation: Partial<IPublication>
+): Promise<void> {
+  const docRef = doc(db, "publications", id);
+  await updateDoc(docRef, publiation);
+}
+
+export async function deletePublciation(id: string): Promise<void> {
+  const docRef = doc(db, "publications", id);
   await deleteDoc(docRef);
 }
