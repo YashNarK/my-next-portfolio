047c151d7d2d7890b1ccb419538ff72c188652b2
YashNarK <narenkrithick@gmail.com>
2025-05-30 14:39:24 +0530

    removed log in firebase-client

--- src/lib/firebase/firebase-client.ts
+++ src/lib/firebase/firebase-client.ts
@@ -15,7 +15,6 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
-console.log("Firebase Config", firebaseConfig);
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
