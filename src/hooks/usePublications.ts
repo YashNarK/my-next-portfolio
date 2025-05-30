e946029bfa3d1d28fecb294551b4a0a919fad711
YashNarK <narenkrithick@gmail.com>
2025-05-30 23:35:28 +0530

    usePublications hook defined

---
+++ src/hooks/usePublications.ts
@@ -0,0 +1,25 @@
+import { getAllPublications } from "@/lib/firebase/firestore-crud";
+import { useQuery } from "@tanstack/react-query";
+import { IPublication } from "../../data/data.type";
+import { AxiosError } from "axios";
+
+export const usePublications = () => {
+  const { data, error, isLoading, isFetching } = useQuery<
+    (IPublication & { id: string })[],
+    AxiosError
+  >({
+    staleTime: 1000 * 60 * 5, // 5 minutes
+    gcTime: 1000 * 60 * 60, // 1 hour,
+    placeholderData: (previousData) => previousData,
+    queryKey: ["publications"],
+    queryFn: getAllPublications,
+  });
+
+  return {
+    data: data,
+    error: error?.message,
+    isLoading: isLoading || isFetching,
+  };
+};
+
+export default usePublications;
