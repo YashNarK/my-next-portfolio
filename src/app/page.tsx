5f348b224777295268e43d7154bb93b2517711e3
YashNarK <narenkrithick@gmail.com>
2025-06-10 18:02:20 +0530

    minHeight={"100%"}

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Banner from "@/components/homePageComponents/Banner";
 import { Box } from "@mui/material";
 export default function Home() {
   return (
-    <Box m={0} p={0} height={"100%"} className="home">
+    <Box m={0} p={0} minHeight={"100%"} className="home">
       <Banner />
     </Box>
   );
