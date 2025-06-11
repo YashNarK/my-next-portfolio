77f688731da9a38fbc9c85338e53d58b5706d6a0
YashNarK <narenkrithick@gmail.com>
2025-06-11 08:53:45 +0530

    added Skeleton to publication

---
+++ src/app/publications/PublicationTileSkeleton.tsx
@@ -0,0 +1,38 @@
+import { Grid, Box, Typography, Skeleton } from "@mui/material";
+import React from "react";
+
+const PublicationTileSkeleton = ({ key }: { key: number }) => {
+  return (
+    <Grid
+      size={{ xs: 12, sm: 6 }}
+      key={key}
+      border={"2px solid gray"}
+      borderRadius={"10px"}
+    >
+      <Skeleton variant="text" sx={{ fontSize: "1rem" }} />
+      <Box
+        display="flex"
+        flexDirection={"row"}
+        alignItems={"center"}
+        justifyContent={"center"}
+      >
+        <Skeleton
+          variant="circular"
+          width={60}
+          height={60}
+          sx={{ display: "inline-block", mx: 4 }}
+        />
+        <Typography
+          variant="handWritten"
+          sx={{ display: "inline", fontSize: "2rem" }}
+        >
+          Loading Publications...
+        </Typography>
+      </Box>
+      <Skeleton variant="rectangular" height={60} sx={{ my: 3 }} />
+      <Skeleton variant="rounded" height={60} />
+    </Grid>
+  );
+};
+
+export default PublicationTileSkeleton;
--- src/app/publications/page.tsx
+++ src/app/publications/page.tsx
@@ -1,25 +1,30 @@
 "use client";
 
 import usePublications from "@/hooks/usePublications";
-import { Box, Grid } from "@mui/material";
+import { Box, Grid, Skeleton, Typography } from "@mui/material";
 import PublicationTile from "./PublicationTile";
+import PublicationTileSkeleton from "./PublicationTileSkeleton";
 
 const Publications = () => {
   const { data: publicationsList, error, isLoading } = usePublications();
 
   return (
-    <Box height={"100%"} px={4}>
+    <Box minHeight={"100%"} px={4}>
       <Grid container mt={10} spacing={3} gap={3}>
-        {(publicationsList || []).map((publication, index) => (
-          <Grid
-            size={{ xs: 12, sm: 6 }}
-            key={index}
-            border={"2px solid gray"}
-            borderRadius={"10px"}
-          >
-            <PublicationTile publication={publication} />
-          </Grid>
-        ))}
+        {isLoading
+          ? [0, 1, 2, 3].map((_, index) => (
+              <PublicationTileSkeleton key={index} />
+            ))
+          : (publicationsList || []).map((publication, index) => (
+              <Grid
+                size={{ xs: 12, sm: 6 }}
+                key={index}
+                border={"2px solid gray"}
+                borderRadius={"10px"}
+              >
+                <PublicationTile publication={publication} />
+              </Grid>
+            ))}
       </Grid>
     </Box>
   );
