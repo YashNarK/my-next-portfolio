ca0e242336bce8d7d01459120012938606cd0ddb
YashNarK <narenkrithick@gmail.com>
2025-06-11 08:37:18 +0530

    while uploading files we set their cacheControl

--- src/lib/firebase/uploadFiles.ts
+++ src/lib/firebase/uploadFiles.ts
@@ -3,7 +3,11 @@ import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 export async function uploadImage(file: File, path: string): Promise<string> {
   const storageRef = ref(storage, `images/${path}`);
-  await uploadBytes(storageRef, file);
+  const metadata = {
+    cacheControl: "public, max-age=31536000", // Example: Cache for 1 year
+  };
+
+  await uploadBytes(storageRef, file, metadata);
   const url = await getDownloadURL(storageRef);
   return url;
 }
