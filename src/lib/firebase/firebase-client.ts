850f918df1b57e8dc30c942bd1036c5454040694
YashNarK <narenkrithick@gmail.com>
2025-06-13 13:02:56 +0530

    added auth configs

--- src/lib/firebase/firebase-client.ts
+++ src/lib/firebase/firebase-client.ts
@@ -1,6 +1,8 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
+import { getAuth as getAdminAuth } from "firebase-admin/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,6 +19,7 @@ const firebaseConfig = {
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
-
+export const auth = getAuth(app);
+export const adminAuth = getAdminAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
