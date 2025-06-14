eddd571d9d0f203f403717d138004817ba063cc3
YashNarK <narenkrithick@gmail.com>
2025-06-14 16:09:02 +0530

    modifed fonst in login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -40,29 +40,19 @@ export default function LoginPage() {
   };
 
   return (
-    // <div style={{ padding: 40 }}>
-    //   <input
-    //     value={email}
-    //     onChange={(e) => setEmail(e.target.value)}
-    //     placeholder="Email"
-    //   />
-    //   <input
-    //     value={password}
-    //     onChange={(e) => setPassword(e.target.value)}
-    //     placeholder="Password"
-    //     type="password"
-    //   />
-    //   <button onClick={login}>Login</button>
-    // </div>
     <Box
-      mt={20}
       minHeight="100%"
       display="flex"
       justifyContent="center"
       alignItems="center"
     >
-      <Paper elevation={6} sx={{ padding: 4, width: 350, borderRadius: 3 }}>
-        <Typography variant="h5" fontWeight="bold" textAlign="center" mb={3}>
+      <Paper elevation={6} sx={{ padding: 2, width: 350, borderRadius: 3 }}>
+        <Typography
+          variant="codeLike"
+          fontWeight="bold"
+          textAlign="center"
+          mb={3}
+        >
           Admin Login
         </Typography>
 
@@ -74,7 +64,6 @@ export default function LoginPage() {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           margin="normal"
- 
         />
 
         <TextField
@@ -97,6 +86,9 @@ export default function LoginPage() {
           Login
         </Button>
       </Paper>
+      <Typography>
+        
+      </Typography>
     </Box>
   );
 }
